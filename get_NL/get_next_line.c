4b8279e7eda53bfcef45e39288d72ad0f4c5298d
nmaila42 <50695873+nmaila42@users.noreply.github.com>
2019-07-08 14:05:45 +0200

    Add files via upload

---
+++ get_NL/get_next_line.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaila <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/06 12:28:01 by nmaila            #+#    #+#             */
+/*   Updated: 2019/07/06 15:37:34 by nmaila           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static int		new_line(char **s, char **line, int fd, int ret)
+{
+	char		*tmp;
+	int			len;
+
+	len = 0;
+	while (s[fd][len] != '\n' && s[fd][len] != '\0')
+		len++;
+	if (s[fd][len] == '\n')
+	{
+		*line = ft_strsub(s[fd], 0, len);
+		tmp = ft_strdup(s[fd] + len + 1);
+		free(s[fd]);
+		s[fd] = tmp;
+		if (s[fd][0] == '\0')
+			ft_strdel(&s[fd]);
+	}
+	else if (s[fd][len] == '\0')
+	{
+		*line = ft_strdup(s[fd]);
+		ft_strdel(&s[fd]);
+		if (ret == BUFF_SIZE)
+			return (0);
+	}
+	return (1);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char	*s[250];
+	char		buf[BUFF_SIZE + 1];
+	char		*temp;
+	int			ret;
+
+	if (fd < 0 || line == NULL)
+		return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		if (s[fd] == NULL)
+			s[fd] = ft_strnew(1);
+		temp = ft_strjoin(s[fd], buf);
+		free(s[fd]);
+		s[fd] = temp;
+		if (ft_strchr(s[fd], '\n'))
+			break ;
+	}
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
+		return (0);
+	return (new_line(s, line, fd, ret));
+}
---
+++ get_NL/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaila <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/30 16:11:27 by nmaila            #+#    #+#             */
+/*   Updated: 2019/07/06 15:37:39 by nmaila           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
