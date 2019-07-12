29b83cd1550e656e58fdd24a4c46f2b70c710faf
nmaila42 <50695873+nmaila42@users.noreply.github.com>
2019-07-12 16:47:12 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,47 +6,37 @@
 /*   By: nmaila <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 14:50:12 by nmaila            #+#    #+#             */
-/*   Updated: 2019/07/12 15:41:44 by nmaila           ###   ########.fr       */
+/*   Updated: 2019/07/12 16:31:51 by nmaila           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-/*
-static char			ft_joinfree(char **str,  char buff, int fd)
-{
-	char		*temp;
 
-	temp = ft_strjoin (str, nuff);
-	free(str[fd]);
-	str[fd] = temp;
-	return (str[fd]);
-}
-*/
-static int			ft_newline(char **str, char **line, int fd, int bytes)
+static int		ft_newline(char **str, char **line, int fd, int bytes)
 {
 	char		*temp;
-	int		i;
+	int			i;
 
 	i = 0;
-    while (str[fd][i] != '\n' && str[fd][i] != '\0')
-        i++;
+	while (str[fd][i] != '\n' && str[fd][i] != '\0')
+		i++;
 	if (str[fd][i] == '\n')
-    {
-        *line = ft_strsub(str[fd], 0, i);
-        temp = ft_strdup(str[fd] + i + 1);
-        free(str[fd]);
-        str[fd] = temp;
-        if (str[fd][0] == '\0')
-            ft_strdel(&str[fd]);
-    }
+	{
+		*line = ft_strsub(str[fd], 0, i);
+		temp = ft_strdup(str[fd] + i + 1);
+		free(str[fd]);
+		str[fd] = temp;
+		if (str[fd][0] == '\0')
+			ft_strdel(&str[fd]);
+	}
 	else if (str[fd][i] == '\0')
 	{
 		*line = ft_strdup(str[fd]);
 		ft_strdel(&str[fd]);
-        if (bytes == BUFF_SIZE)
-            return (0);
-    }
-    return (1);
+		if (bytes == BUFF_SIZE)
+			return (0);
+	}
+	return (1);
 }
 
 int				get_next_line(const int fd, char **line)
@@ -63,16 +53,15 @@ int				get_next_line(const int fd, char **line)
 		buff[bytes] = '\0';
 		if (str[fd] == NULL)
 			str[fd] = ft_strnew(0);
-		//ft_joinfree(str[fd], buff);
 		temp = ft_strjoin(str[fd], buff);
-		free (str[fd]);
+		free(str[fd]);
 		str[fd] = temp;
 		if (ft_strchr(str[fd], '\n'))
 			break ;
 	}
 	if (bytes < 0)
 		return (-1);
-	else if (bytes == 0 && (str[fd] == NULL || str[fd][0] =='\0'))
+	else if (bytes == 0 && (str[fd] == NULL || str[fd][0] == '\0'))
 		return (0);
 	return (ft_newline(str, line, fd, bytes));
 }
